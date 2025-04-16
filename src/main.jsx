5487b2f38ed1a86b6ab5e6ae55f1d7b6be8c6f7f
ganeshsilla2004 <ganeshsilla2004@gmail.com>
2025-04-16 08:10:39 +0530

    url cheged

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,8 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_kLKTFcTYZ",
   client_id: "2lokd7f8a5tetaeamk775g32ha",
-  redirect_uri: "http://localhost:5173",
+  redirect_uri: "https://main.dlicpw3zl70pz.amplifyapp.com",
   response_type: "code",
-  // Changed to only include scopes typically enabled by default in Cognito
   scope: "openid email",
   
   loadUserInfo: true,
